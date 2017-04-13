d3e73ac97914089286017876a1da745b025d95b9
miaopu1990 <miaopu1990>
2017-04-13 10:55:24 +0800

    111

---
+++ SphereMenu-master 2/SphereMenu/ViewController.m
@@ -0,0 +1,55 @@
+//
+//  ViewController.m
+//  SphereMenu
+//
+//  Created by Tu You on 14-8-24.
+//  Copyright (c) 2014年 TU YOU. All rights reserved.
+//
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
+
+#import "ViewController.h"
+#import "SphereMenu.h"
+
+@interface ViewController () <SphereMenuDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithRed:1 green:0.58 blue:0.27 alpha:1];
+    
+    UIImage *startImage = [UIImage imageNamed:@"start"];
+    UIImage *image1 = [UIImage imageNamed:@"icon-twitter"];
+    UIImage *image2 = [UIImage imageNamed:@"icon-email"];
+    UIImage *image3 = [UIImage imageNamed:@"icon-facebook"];
+    NSArray *images = @[image1, image2, image3];
+    SphereMenu *sphereMenu = [[SphereMenu alloc] initWithStartPoint:CGPointMake(160, 320)
+                                                         startImage:startImage
+                                                      submenuImages:images];
+    sphereMenu.delegate = self;
+    [self.view addSubview:sphereMenu];
+}
+
+- (void)sphereDidSelected:(int)index
+{
+    NSLog(@"sphere %d selected", index);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+@end
